6ee9cb2eb17db2a25ccd913d4b19b06a577b98fd
andrenprestes <andren.p@hotmail.com>
2022-02-10 13:34:37 -0300

    do primeiro video falta do websocket pra frente

--- BackEnd/src/controllers/CreatePartidaController.js
+++ BackEnd/src/controllers/CreatePartidaController.js
@@ -5,13 +5,12 @@ class CreatePartidaController{
     async handle(request, response){
         const { partida } = request.body
 
-        console.log(partida);
         const {id} = request;
 
         const service = new CreatePartidaService();
 
         const result = await service.execute("1", 10, id, id);
-
+        
         return response.json(result);
     }
 }
