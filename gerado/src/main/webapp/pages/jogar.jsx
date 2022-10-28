0043c550132cb3d113dfaa89466052adcd9b83cf
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2022-10-28 16:09:34 -0300

    Alterações do Template do Partial React - parte 5

--- gerado/src/main/webapp/pages/jogar.jsx
+++ gerado/src/main/webapp/pages/jogar.jsx
@@ -18,7 +18,7 @@ class jogar extends React.Component
 	
 	render() {  
       return (
-        <div><input type="checkbox" id="head">{{ head }}</input type="checkbox"><span id="user.nome">{{ user.nome }}</span><span id="opponent">{{ opponent }}</span><img src="user.picture"></img><img src="img2"></img><Head ></Head>
+        <div><head ></head><span id="user.nome">{{ user.nome }}</span><span id="opponent">{{ opponent }}</span><img src="user.picture"></img><img src="img2"></img><Head ></Head>
 	
 	<button onClick="opponet">opponet</button>
 	
